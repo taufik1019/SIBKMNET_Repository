70ba000e74466f1325ac36348965cbd08a08899c
taufik1019 <taufikandrirahman17@gmail.com>
2022-10-03 15:10:08 +0700

    Repository

---
+++ SIBKMNET_WebApps/Controllers/AccountController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SIBKMNET_WebApps.Repositories.Data;
+using SIBKMNET_WebApps.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SIBKMNET_WebApps.Controllers
+{
+    public class AccountController : Controller
+    {
+        AccountRepository accountRepository;
+
+        public AccountController(AccountRepository accountRepository)
+        {
+            this.accountRepository = accountRepository;
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login(Login login)
+        {
+            // statement mengambil data dari database sesuai dengan email dan password
+            var data = accountRepository.Login(login);
+            if(data != null)
+            {
+                HttpContext.Session.SetString("Role", data.Role);
+                return RedirectToAction("Index", "Province");
+            }
+            return View();
+            // return -> Id Employee, FullName, Email, Role -> Viewmodels
+            // inisialisasi nilai padaa session
+
+        }
+    }
+}
---
+++ SIBKMNET_WebApps/Repositories/Data/AccountRepository.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SIBKMNET_WebApps.Context;
+using SIBKMNET_WebApps.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SIBKMNET_WebApps.Repositories.Data
+{
+    public class AccountRepository
+    {
+        MyContext myContext;
+
+        public AccountRepository(MyContext myContext)
+        {
+            this.myContext = myContext;
+        }
+
+        public ResponseLogin Login(Login login)
+        {
+         var data = myContext.UserRoles
+                .Include(x => x.Role)
+                .Include(x => x.User)
+                .Include(x => x.User.Employee)
+                .FirstOrDefault(x => x.User.Employee.Email.Equals(login.Email) &&
+                                     x.User.Password.Equals(login.Password));
+            if( data !=null)
+            {
+                var response = new ResponseLogin()
+                {
+                    Id = data.User.Employee.Id,
+                    FullName = data.User.Employee.FullName,
+                    Email = data.User.Employee.Email,
+                    Role = data.Role.Name
+                };
+                return response;
+            }
+            return null;
+        }
+    }
+}
